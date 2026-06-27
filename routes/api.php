2071c50251b105b45a498fcdf0ce2e1ebd225879
Amitav Roy <reachme@amitavroy.com>
2026-06-27 14:47:57 +0530

    feat: the api is now working

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\OrderStatusController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/order-status', OrderStatusController::class)->middleware('shop.token');
